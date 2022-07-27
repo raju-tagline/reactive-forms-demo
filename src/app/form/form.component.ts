99752ee61fe56c4c1a5a84e4a90455f67c4814de
raju-tagline <raju.tagline@gmail.com>
2022-07-27 17:31:06 +0530

    Reactive forms with json file data basic all validations and show data into console done.

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -11,13 +11,15 @@ export class FormComponent implements OnInit {
   public productDetailList: any = Object.values(productDetails)[3];
   public categoryList: any;
   public subCategoryList: any;
-  public errorMinPrice: any;
-  public errorMaxDiscount: any;
-  public errorSubCategoryMessage: any;
+  public errorMinPrice: any = '';
+  public errorMaxDiscount: any = '';
+  public errorSubCategoryMessage: any = '';
   public finalProduct: any;
   public commaMinPrice: any;
   public commaMaxDiscount: any;
   public subCatRequired: boolean = false;
+  public isMinPrice: boolean = false;
+  public isMaxDiscount: boolean = false;
 
   constructor() {}
 
@@ -32,18 +34,9 @@ export class FormComponent implements OnInit {
     this.productForm = new FormGroup({
       product: new FormControl('', Validators.required),
       category: new FormControl('', Validators.required),
-      subCategory: new FormControl(
-        '',
-        this.subCatRequired ? Validators.required : null
-      ),
-      min_price: new FormControl('', [
-        Validators.required,
-        Validators.pattern('^[0-9]*$'),
-      ]),
-      max_discount: new FormControl('', [
-        Validators.required,
-        Validators.pattern('^[0-9]*$'),
-      ]),
+      subCategory: new FormControl(''),
+      min_price: new FormControl('', [Validators.required]),
+      max_discount: new FormControl('', [Validators.required]),
     });
   }
 
@@ -85,10 +78,13 @@ export class FormComponent implements OnInit {
       (res: any) => res.key === event.target.value
     );
     if (this.categoryList[index].is_subcat_required) {
+      this.accessSubCategory.addValidators(Validators.required);
+      this.accessSubCategory.updateValueAndValidity();
       this.subCatRequired = true;
       this.errorSubCategoryMessage = 'Sub-category is required!';
-      this.productForm.invalid;
     } else {
+      this.accessSubCategory.clearValidators();
+      this.accessSubCategory.updateValueAndValidity();
       this.subCatRequired = false;
       this.errorSubCategoryMessage = '';
     }
@@ -118,18 +114,30 @@ export class FormComponent implements OnInit {
         }
       });
       const values: any = Number(event.target.value.replace(/,/g, ''));
-      if (values <= index[0].min_price) {
-        this.errorMinPrice = `Minimum price of product is ${index[0].min_price}`;
-        this.productForm.invalid;
+      if (values <= index[0]?.min_price) {
+        this.isMinPrice = true;
       } else {
+        this.isMinPrice = false;
+      }
+      this.isMinPrice = false;
+      if (values <= index[0]?.min_price) {
+        this.accessMinPrice.addValidators(Validators.min(index[0]?.min_price));
+        this.accessMinPrice.updateValueAndValidity();
+        this.errorMinPrice =
+          this.accessMinPrice?.errors?.['required'] &&
+          this.accessMinPrice?.touched &&
+          !this.accessMinPrice?.value?.length
+            ? ''
+            : `Minimum price of product is ${index[0]?.min_price}`;
+      } else {
+        // this.accessMinPrice.removeValidators(
+        //   Validators.min(this.accessMaxDiscount.errors?.['min']?.['min'])
+        // );
+        this.accessMinPrice.clearValidators();
+        this.accessMinPrice.updateValueAndValidity();
         this.errorMinPrice = '';
       }
     }
-    //   this.errorMinPrice = '';
-    // } else {
-    //   this.errorMinPrice = 'Please select above dropdown!';
-    //   return (event.target.value = null);
-    // }
   }
 
   /**
@@ -147,10 +155,23 @@ export class FormComponent implements OnInit {
         }
       });
       const values: any = Number(event.target.value.replace(/,/g, ''));
-      if (values >= index[0].max_discount) {
-        this.errorMaxDiscount = `Maxmum discount of product is ${index[0].max_discount}`;
-        this.productForm.invalid;
+      if (values >= index[0]?.max_discount) {
+        this.accessMaxDiscount.addValidators(
+          Validators.max(index[0]?.max_discount)
+        );
+        this.accessMaxDiscount.updateValueAndValidity();
+        this.errorMaxDiscount =
+          this.accessMaxDiscount?.errors?.['required'] &&
+          this.accessMaxDiscount?.touched &&
+          !this.accessMaxDiscount?.value?.length
+            ? ''
+            : `Maxmum discount of product is ${index[0]?.max_discount}`;
       } else {
+        // this.accessMaxDiscount.removeValidators(
+        //   Validators.max(this.accessMaxDiscount.errors?.['max']?.['max'])
+        // );
+        this.accessMaxDiscount.clearValidators();
+        this.accessMaxDiscount.updateValueAndValidity();
         this.errorMaxDiscount = '';
       }
     }
@@ -160,16 +181,24 @@ export class FormComponent implements OnInit {
    * onSubmit
    */
   public onSubmit() {
-    if (
-      !this.productForm.valid &&
-      this.subCatRequired &&
-      !this.subCatRequired
-    ) {
+    if (!this.productForm.valid) {
       return;
     } else {
+      this.subCatRequired = false;
+      this.categoryList = [];
+      this.subCategoryList = [];
+      this.accessSubCategory.clearValidators();
+      this.accessMinPrice.clearValidators();
+      this.accessMaxDiscount.clearValidators();
+      this.accessSubCategory.updateValueAndValidity();
+      this.accessMinPrice.updateValueAndValidity();
+      this.accessMaxDiscount.updateValueAndValidity();
       console.log('Product Details :::>> ', this.productForm.value);
-      // this.finalProduct = this.productForm.value;
       this.productForm.reset();
+      this.accessMinPrice.addValidators(Validators.required);
+      this.accessMaxDiscount.addValidators(Validators.required);
+      this.accessMinPrice.updateValueAndValidity();
+      this.accessMaxDiscount.updateValueAndValidity();
     }
   }
 }
